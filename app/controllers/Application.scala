4e6295573e91693dad4b59144d8cb5f9e3e7c2f5
Anders Bohn Jespersen <abj@lenio.dk>
2014-05-23 21:47:03 +0200

    more me

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,16 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
+import play.api.i18n.Messages
 
 object Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def test = Action {
+    Ok(views.html.index("d->"+Messages("description")))
   }
 
 }
